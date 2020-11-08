63daa8f0c0ece8bcedb079dc118edb34df7ba85e
Roberto Montes de Oca <robbiemdo79@gmail.com>
2020-11-08 11:23:52 -0400

    Primer Commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,23 @@
+var socket = io();
+
+        // Escuchar
+        socket.on('connect', function(){
+            console.log('Conectado al servidor')
+        });
+
+        socket.on('disconnect', function() {
+            console.log('Perdimos conexion con el servidor')
+        });
+
+        // Enviar informacion
+        socket.emit('enviarMensaje', {
+            usuario: 'Roberto',
+            mensaje: 'Hola Mundo'
+        }, function(resp) {
+            console.log('respuesta server: ', resp);
+        });
+
+        // Escuchar informacion
+        socket.on('enviarMensaje', function(mensaje) {
+            console.log('Servidor: '+mensaje.mensaje);
+        });
